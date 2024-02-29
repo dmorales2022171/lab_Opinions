33ab0f549ff87624291d4622658da7d0209d2582
dmorales2022171 <dmorales-2022171@kinal.edu.gt>
2024-02-29 03:09:26 -0600

    se corrigio faltas

--- src/publications/publication.controller.js
+++ src/publications/publication.controller.js
@@ -42,26 +42,22 @@ export const publicationPut = async (req, res = response) => {
 }
 
 export const publicationDelete = async (req, res = response) => {
-    const { userName } = req.params; // Obtener el nombre de usuario de los parámetros de la solicitud
+    const { userName } = req.params; 
 
     try {
-        // Buscar al usuario por su nombre de usuario
         const user = await User.findOne({ userName });
-
-        // Verificar si se encontró al usuario
         if (!user) {
             return res.status(404).json({ msg: 'User not found' });
         }
 
-        // Cambiar el estado de las publicaciones del usuario a false
         const updatedPublications = await Publication.updateMany(
-            { author: user._id }, // Buscar publicaciones del usuario por su ID
-            { status: false } // Actualizar el campo 'status' a false
-        );
+            { author: user._id }, 
+            { status: false } 
+        );gi
 
         res.status(200).json({
             msg: 'Publications deleted',
-            publicationsUpdated: updatedPublications.nModified // Número de publicaciones actualizadas
+            publicationsUpdated: updatedPublications.nModified
         });
     } catch (error) {
         console.error(error);
