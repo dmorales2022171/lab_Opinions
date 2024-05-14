999f491f066225951a9748b6a4c6c6921cdfeb87
dmorales2022171 <dmorales-2022171@kinal.edu.gt>
2024-05-14 11:10:59 -0600

    se agrego mas descripciones a las publicaciones

--- configs/mongo.js
+++ configs/mongo.js
@@ -20,9 +20,9 @@ export const dbConnection = async () => {
             const count = await Publication.countDocuments();
             if (count === 0) {
                 const defaultPublications = [
-                    { title: 'Practica supervisada', content: 'Sistema adopcion de mascotas: Api rest el cual tiene como principal el poder adoptar mascotas' },
-                    { title: 'Taller III', content: 'Getor de supermercado: Api rest que tiene como funcionalidesdes basicas de un supermercado' },
-                    { title: 'Tecnologia III', content: 'Contenido: Aprendimos acerca de diferentes tecnologias web' }
+                    { title: 'Practica supervisada', content: 'el "Sistema de Adopción de Mascotas" es más que una simple API: es una herramienta poderosa para conectar a las mascotas necesitadas con hogares amorosos, mientras promueve la responsabilidad y el cuidado de los animales en la comunidad. Con su enfoque centrado en el usuario y sus características intuitivas, esta API está destinada a hacer que el proceso de adopción de mascotas sea más accesible, eficiente y gratificante para todos los involucrados.' },
+                    { title: 'Taller III', content: 'el "Gestor de Supermercado" es una herramienta versátil y poderosa que simplifica la gestión operativa de los supermercados al ofrecer una plataforma centralizada para gestionar inventarios, procesar transacciones y brindar una experiencia de compra fluida y conveniente para los clientes. Con su enfoque en la eficiencia y la facilidad de uso, esta API está diseñada para mejorar la productividad y la experiencia del cliente en el entorno competitivo de la industria minorista.' },
+                    { title: 'Tecnologia III', content: 'nuestro viaje de aprendizaje en tecnologías web fue una experiencia enriquecedora que nos proporcionó una comprensión sólida y diversa de las herramientas y técnicas necesarias para crear aplicaciones web modernas y exitosas. Estamos emocionados de aplicar este conocimiento en proyectos futuros y de seguir explorando las nuevas tendencias y avances en este apasionante campo.' }
                 ];
 
                 for (const publicationData of defaultPublications) {
